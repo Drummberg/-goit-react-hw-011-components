02411b463fcf2356ce806fe48191b19ddc22bcfb
Drummberg <drummberg@gmail.com>
2022-04-28 01:16:15 +0300

    Create TransactionItem.js

---
+++ src/components/Transactions/TransactionItem/TransactionItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function TransactionItem({ type, amount, currency }) {
+  return (
+    <tbody>
+      <tr>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{currency}</td>
+      </tr>
+    </tbody>
+  );
+}
+
+TransactionItem.propTypes = {
+  type: PropTypes.string,
+  amount: PropTypes.string,
+  currency: PropTypes.string,
+};
