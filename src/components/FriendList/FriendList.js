c2a64ff1425c99a113f9f22c86f3a97f25e3f93f
Drummberg <drummberg@gmail.com>
2022-04-27 23:31:56 +0300

    Create FriendList.js

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem/FriendListItem';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => {
+        return <FriendListItem key={friend.id} {...friend} />;
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default FriendList;
