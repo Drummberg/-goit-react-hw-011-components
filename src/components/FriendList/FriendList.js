616a6904144523c82dee2cd31bcfa2cfee19937b
Drummberg <drummberg@gmail.com>
2022-04-29 14:57:27 +0300

    Update FriendList.js

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem/FriendListItem';
+import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={styles.friend_list}>
       {friends.map(friend => {
         return <FriendListItem key={friend.id} {...friend} />;
       })}
