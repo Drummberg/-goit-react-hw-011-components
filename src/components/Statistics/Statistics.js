ea8f4eefc1cc5c11be7239cbc95ebf5fcf706435
Drummberg <drummberg@gmail.com>
2022-04-29 22:43:16 +0300

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+import generateRandomColor from './randomColors';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
-      <ul className="stat-list">
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+      <ul className={styles.stat_list}>
         {stats.map(({ id, label, percentage }) => (
-          <li key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}%</span>
+          <li
+            key={id}
+            style={{
+              backgroundColor: generateRandomColor(),
+            }}
+          >
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
