b13d6e019b46b51da1b10a21c7729db6cba15fb3
Drummberg <drummberg@gmail.com>
2022-04-29 23:14:28 +0300

    Update TransactionHistory.js

--- src/components/Transactions/TransactionHistory.js
+++ src/components/Transactions/TransactionHistory.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TransactionItem from './TransactionItem/TransactionItem';
+import styles from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table class="transaction-history">
+    <table className={styles.transaction_history}>
       <thead>
         <tr>
           <th>Type</th>
