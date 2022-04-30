6c03967a958ce6374da33599f1ed0c8bc07a7f30
Drummberg <drummberg@gmail.com>
2022-05-01 00:49:31 +0300

    Update TransactionHistory.js

--- src/components/Transactions/TransactionHistory.js
+++ src/components/Transactions/TransactionHistory.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TransactionItem from './TransactionItem/TransactionItem';
 import styles from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
     <table className={styles.transaction_history}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+      <thead className={styles.thead}>
+        <tr className={styles.tr}>
+          <th className={styles.th}>Type</th>
+          <th className={styles.th}>Amount</th>
+          <th className={styles.th}>Currency</th>
         </tr>
       </thead>
-      {items.map(item => {
-        return <TransactionItem key={item.id} {...item} />;
-      })}
+      <tbody className={styles.tbody}>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className={styles.tr} key={id}>
+            <td className={styles.td}>{type}</td>
+            <td className={styles.td}>{amount}</td>
+            <td className={styles.td}>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
 
 TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ),
 };
